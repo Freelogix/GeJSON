609421ff7caf1b6d570924a9a987bbbf022fbf7b
Freelogix <main@freelogix.it>
2023-07-27 23:18:29 +0200

    Add parse array out from the object, and debug

--- it/freelogix/gejson/parser/json/GeJSONParser.java
+++ it/freelogix/gejson/parser/json/GeJSONParser.java
@@ -18,13 +18,20 @@ public class GeJSONParser{
 	Tokenizer tokenizer;
 
 	// ---------------------------------------------------------------------------------
-	private boolean debugMode = true;
+	private boolean debugMode = false;
 	
 	public static final int TRACE = 0;
 	public static final int DEBUG = 1;
 	public static final int INFO = 2;
 	public static final int WARNING = 3;
 	public static final int ERROR = 4;
+	
+	public void debugEnable() {
+		debugMode=true;
+	}
+	public void debugDisable() {
+		debugMode=false;
+	}
 	
 	/**
 	 * Internal log, it's for debug, write message to stdout with INFO level
